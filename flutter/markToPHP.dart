aff645e3578f21a349ee9492a4d6fe02ff5f45ed
Carl Grech <carlg@stcmalta.edu.mt>
2026-02-26 19:09:42 +0100

    updated markToPHP for more specific database compatibility

--- flutter/markToPHP.dart
+++ flutter/markToPHP.dart
@@ -1,5 +1,5 @@
 await http.post(
-  Uri.parse("https://yourdomain.com/api/mark_taken.php"),
+  Uri.parse("https://medicaltracker.com/api/mark_taken.php"),
   body: {
     "patient_id": "1",
     "medication_id": "5",
